733948e2804c3c83eb74cf29a81eb5ae9b22a0bc
MadhukaD <madhukad@sltc.ac.lk>
2023-11-23 11:06:43 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+        
+    def firstFunction():
+        num1 = int(input("Enter Number 1: "))
+        num2 = int(input("Enter Number 2: "))
+        
+        addVal = num1 + num2
+        multiVal = num1 * num2
+        
+        print("Addition =", addVal)
+        print("Multiplication =", multiVal)
+        
+    firstFunction()
+
+if __name__ == "__main__":
+    main()
